8720ffad4235ebf0bcecebce78c7ad59b6aba6cb
Thiago b Soares <tbsoaresvalkms@gmail.com>
2017-12-29 14:42:19 -0200

    Correction on integration test

--- src/test/java/com/tbsoaresvalkms/transfer/integration/TransferControllerTest.java
+++ src/test/java/com/tbsoaresvalkms/transfer/integration/TransferControllerTest.java
@@ -3,26 +3,30 @@ package com.tbsoaresvalkms.transfer.integration;
 import com.tbsoaresvalkms.transfer.models.Transfer;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TransferControllerTest {
 
-    private final String URL = "http://localhost:8080/transfer";
-    private TestRestTemplate testRestTemplate;
+    private final String URL = "/transfer";
 
-    @Before
-    public void init() {
-        testRestTemplate = new TestRestTemplate();
-    }
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     public void itShouldReturnTransferCreated() {
